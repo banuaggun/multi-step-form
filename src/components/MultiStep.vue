cfe8a279d4954e431802f00f9eeb987a15475e5c
banuaggun <banuaggun@outlook.com>
2024-07-08 21:36:46 +0300

    Project environment preparations continue - 2

---
+++ src/components/MultiStep.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="card-steps-container">
+      <Steps />
+    </div>
+    <div class="card-steps-form-details">
+      card
+    </div>
+  </div>
+</template>
+<script setup>
+import Steps from './Steps.vue'
+
+</script>
+<style lang="scss">
+  
+</style>
