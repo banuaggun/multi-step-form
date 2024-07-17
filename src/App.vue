69ac7e67d64b8fa52fa18e3c025a3e8f635cdf26
banuaggun <banuaggun@outlook.com>
2024-07-17 23:04:16 +0300

    form steps started to be created

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default{
 }
 </script>
 
-<style>
+<style lang="scss">
 @import './styles/global/global.sass';
 
 </style>
--- src/components/Steps.vue
+++ src/components/Steps.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>
-    forms
+  <div class="steps" v-for="step in steps" :key="step">
+    <div class="steps-1 dot">
+      {{step.id}}
+    </div>
+      <div class="dot-details">
+        <span>
+          {{step.title}}
+        </span>
+        <p>
+          {{step.stepTitle}}
+        </p>
+      </div>
   </div>
 </template>
-<script>
-export default {
-  
-}
+<script setup>
+const steps = [
+  {
+    id:1,
+    title:"Step 1",
+    stepTitle:"Your Info"
+  },
+  {
+    id:2,
+    title:"Step 2",
+    stepTitle:"Select Plan"
+  },
+  {
+    id:3,
+    title:"Step 3",
+    stepTitle:"Add Ons"
+  },
+  {
+    id:4,
+    title:"Step 4",
+    stepTitle:"Summary"
+  },
+]
 </script>
 <style lang="">
   
