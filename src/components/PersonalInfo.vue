6ebefb2371516f37e8c61d85e03a09ea5c683fe9
banuaggun <banuaggun@outlook.com>
2024-08-01 00:35:02 +0300

    Personal information form field is being prepared-2

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -3,6 +3,20 @@
     <h1>Personal Info</h1>
     <p>Please, provide your name, email and phone number</p>
   </div>
+  <form>
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="name" placeholder="Aria Doe">
+    </div>
+    <div class="form-group">
+      <label for="name">Email Address</label>
+      <input type="text" id="email" v-model="name" placeholder="Aria Doe">
+    </div>
+    <div class="form-group">
+      <label for="name">Phone Number</label>
+      <input type="number" id="number" v-model="name" placeholder="Aria Doe">
+    </div>
+  </form>
 </template>
 <script setup>
 
