ffa2af365b80e23f3dae4d51493d8d1998c3471b
banuaggun <banuaggun@outlook.com>
2024-08-14 20:57:52 +0300

    Style features are being made operational.

--- src/components/PlansInfo.vue
+++ src/components/PlansInfo.vue
@@ -3,21 +3,27 @@
     <h1>Select Your Plan</h1>
     <p>You have the option of monthly or yearly billing.</p>
   </div>
-  <form class="form-group-plans">
-      <div class="form-group-plans-radio" v-for="item in planList" :key="item.id">
-        <div class="form-group-plans-radio-ui">
-          <label :for="item.inputID">
-          <img :src="item.planImage" :alt="item.planName">
-        </label>
-        <input type="radio" class="form-group-plans-radio-input" :id="item.inputID" name="promo" v-model="formPlan" :value="item">
-        </div>
+  <form>
+
+    <div class="form-group-select">
+
+      <div class="form-group-select-radio" v-for="item in planList" :key="item.id">
+        <input type="radio" class="form-group-select-radio-input" :id="item.inputID" name="promo" v-model="formPlan" :value="item">
+          <label :for="item.inputID" class="form-group-select-radio-ui">
+            <img :src="item.planImage" :alt="item.planName">
+            
+            <div class="form-group-select-radio-ui-text">
+              <p>{{item.planName}}</p>
+              <span>{{item.planPrice.monthly}}/mo</span>
+            </div>
+          </label>
+
         
         
-        <div class="form-group-plans-text">
-            <p>{{item.planName}}</p>
-            <span>{{item.planPrice.monthly}}/mo</span>
-          </div>
       </div>
+
+    </div>
+      
   </form>
 </template>
 <script setup>
@@ -60,17 +66,62 @@ const planList = ref([
 <style lang="scss">
 @import "../styles/global/global.sass";
 
-  .form-group-plans{
+  .form-group-select{
     display:flex;
     justify-content: space-between;
-    border:1px solid $cardinal;
+    margin-bottom:30px;
 
     &-radio{
-      border:1px solid $black;
-      display:flex;
-      flex-direction:column;
-      width:30%;
+      position:relative;
+      cursor:pointer;
+
+      &-input{
+        position:absolute;
+        width:100%;
+        height:100%;
+        left:0;
+        top:0;
+        z-index:2;
+        cursor:pointer;
+        opacity:0;
+      }
+
+      &-ui{
+        height:150px;
+        width:130px;
+        border:1px solid $beige;
+        padding:12px;
+        border-radius:8px;
+        display:flex;
+        flex-direction:column;
+        justify-content:space-between;
+
+        img{
+          width:35px;
+          height:35px;
+        }
+
+        &-text{
+          p{
+            font-weight:bold;
+            color:$lapis-lazuli;
+            margin-bottom:3px;
+          }
+          span{
+            color:$turquoise;
+            font-size:14px;
+          }
+        }
+      }
+
+      
     }
+
   }
+
+  .form-group-select-radio-input:checked{
+      border:1px solid $lapis-lazuli;
+      background-color:$alice-blue;
+    }
 
 </style>
