e82cab335558e975d258884e4b1f6ec83df3dcd7
banuaggun <banuaggun@outlook.com>
2024-09-05 12:09:07 +0300

    Refactored to Sass - 3

--- src/components/AddOnsInfo.vue
+++ src/components/AddOnsInfo.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="steps-description">
+  <div class="card-form-details-description">
     <h1>Pick Add-Ons</h1>
     <p>Add-ons help enhance your going experiment.</p>
   </div>
   <form>
-    <div class="form-group-check" v-for="item in addOnsList" :key="item.id">
-      <label class="form-group-check-container">
+    <div class="forms">
+
+    <div class="forms-area-check" v-for="item in addOnsList" :key="item.id">
+      <label class="forms-area-check-container">
         <input type="checkbox">
         <span class="checkbox-icon"></span>
-        <div class="form-group-check-container-adds-details">
-          <div class="form-group-check-container-adds-details-text">
+        <div class="forms-area-check-container-adds-details">
+          <div class="forms-area-check-container-adds-details-text">
             <p>{{item.addName}}</p>
             <span>{{item.description}}</span>
           </div>
-          <p class="form-group-check-container-adds-details-price">${{item.planPrice.monthly}}/mo</p>
+          <p class="forms-area-check-container-adds-details-price">${{item.planPrice.monthly}}/mo</p>
         </div>
       </label>
     </div>
-    <div class="form-buttons">
-      <button class="form-buttons-btn form-buttons-btn-secondary">
-        Go Back
+
+    </div>
+
+    <div class="forms-buttons">
+      <button class="forms-buttons-secondary forms-buttons-btn">
+        Back
       </button>
-      <button class="form-buttons-btn form-buttons-btn-primary">
-        Next step
+      <button class="forms-buttons-primary forms-buttons-btn">
+        Next
       </button>
     </div>
   </form>
@@ -69,98 +74,125 @@ const addOnsList = [
 
 <style lang="scss">
 @import '../styles/global/global.sass';
-
-.form-group-check{
-  display:block;
+form{
+  display:flex;
+  flex-direction: column;
+  justify-content:space-between;
   position:relative;
-  cursor:pointer;
-  border:1px solid $cardinal;
-  padding:15px 20px;
-  border-radius:8px;
-  background:$ghost-white;
-  margin-bottom:20px;
+  margin-bottom:0;
+  height:100%;
 
-  &-container{
-    cursor: pointer;
+  .forms{
+    display:flex;
+    flex-direction: column;
+    margin-bottom:20px;
+    margin-top:30px;
 
-    input{
-      position:absolute;
-      left:0;
-      top:0;
-      opacity:0;
-    }
-    .checkbox-icon{
-      width:24px;
-      height:24px;
-      position:absolute;
-      background-color:$white;
-      border-radius:5px;
-      border:1px solid $cool-gray;
-      top:50%;
-      transform:translateY(-50%);
-      transition:all 0.3s;
+    &-area-check{
+      display:block;
+      position:relative;
+      cursor:pointer;
+      border:1px solid $light-beige;
+      padding:15px 20px;
+      border-radius:8px;
+      background:$ghost-white;
+      margin-bottom:10px;
+      
+      &-container{
+        cursor: pointer;
 
-      &::after{
-        content:"";
-        position:absolute;
-        opacity:0;
-        left:8px;
-        top:4px;
-        height:8px;
-        width:5px;
-        border:solid $white;
-        border-width:0 3px 3px 0;
-        transform:rotate(-45deg);
-        transition:all 0.1s;
+        input{
+          position:absolute;
+          left:0;
+          top:0;
+          opacity:0;
+        }
+
+        .checkbox-icon{
+          width:24px;
+          height:24px;
+          position:absolute;
+          background-color:$white;
+          border-radius:5px;
+          border:1px solid $cool-gray;
+          top:50%;
+          transform:translateY(-50%);
+          transition:all 0.3s;
+
+          &::after{
+            content:"";
+            position:absolute;
+            opacity:0;
+            left:8px;
+            top:4px;
+            height:8px;
+            width:5px;
+            border:solid $white;
+            border-width:0 3px 3px 0;
+            transform:rotate(-45deg);
+            transition:all 0.1s;
+          }
+        }
+
+        &-adds-details{
+          padding-left:35px;
+          display:flex;
+          align-items:center;
+          justify-content:space-between;
+
+          &-text{
+            p{
+              font-weight:bold;
+              color:$lapis-lazuli;
+              margin-bottom:3px;
+            }
+
+            span{
+              color:$cool-gray;
+              font-size:14px;
+            }
+          }
+
+          &-price{
+            font-size:14px;
+            font-weight:400;
+            color:$lapis-lazuli;
+          }
+        }
       }
+
     }
 
-    &-adds-details{
-      padding-left:35px;
+    &-buttons{
+      width:100%;
       display:flex;
+      justify-content: space-between;
+      align-content:flex-end;
       align-items:center;
-      justify-content:space-between;
-
-      &-text{
-        p{
-          font-weight:bold;
-          color:$lapis-lazuli;
-          margin-bottom:3px;
-        }
-
-        span{
-          color:$cool-gray;
-          font-size:14px;
-        }
-      }
-
-      &-price{
-        font-size:14px;
-        font-weight:400;
-        color:$lapis-lazuli;
-      }
     }
   }
+ 
 }
 
-.form-group-check-container:hover input ~ .checkbox-icon{
+
+.forms-area-check-container:hover input ~ .checkbox-icon{
   background: $white;
 }
 
-.form-group-check-container input:checked ~ .checkbox-icon{
+.forms-area-check-container input:checked ~ .checkbox-icon{
   background:$lapis-lazuli;
 }
 
-.form-group-check-container input:checked ~ .checkbox-icon::after{
+.forms-area-check-container input:checked ~ .checkbox-icon::after{
   opacity:1;
   transform: rotate(45deg);
 }
 
-.form-group-check-container input:checked ~ .checkbox-icon{
+.forms-area-check-container input:checked ~ .checkbox-icon{
   border:none;
 }
 
-.form-group-check:has(input:checked){
+.forms-area-check:has(input:checked){
   background:$alice-blue;
   border:1px solid $beige;
 }
---
+++ src/components/Summary.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="card-form-details-description">
+    <h1>Finishing Up</h1>
+    <p>Double check everything looks OK before confirming.</p>
+  </div>
+  <form>
+    <div class="forms">
+      <div class="forms-area-summary">
+        <div class="forms-area-summary-select">
+          <div class="bill-state">
+            <div>
+              <p>
+                Arcade
+                <span>(Monthly)</span>
+              </p>
+              <a role="button">Change</a>
+            </div>
+            <div class="bill-state-price">$9/mo</div>
+          </div>
+          
+          <div class="add-ons">
+            <p class="add-ons-name">
+              Online Service
+            </p>
+            <p class="add-ons-price">
+              +$1/mo
+            </p>
+          </div>
+
+          <div class="storage">
+            <p class="storage-name">
+              Larger Storage
+            </p>
+            <p class="storage-price">
+              +$2/mo
+            </p>
+          </div>
+
+          
+        </div>
+        <div class="total">
+            <span>Total (per month)</span>
+            <p>+$12/mo</p>
+          </div>
+      </div>
+
+      <div class="forms-buttons">
+        <button class="forms-buttons-secondary forms-buttons-btn">
+          Back
+        </button>
+        <button class="forms-buttons-primary forms-buttons-btn">
+          Confirm
+        </button>
+      </div>
+    </div>
+  </form>
+</template>
+<script setup>
+</script>
+<style lang="scss">
+@import '../styles/global/global.sass';
+
+form{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position:relative;
+  margin-bottom:0;
+  height:100%;
+
+  .forms{
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    margin-bottom:20px;
+    margin-top:20px;
+
+    &-area-summary{
+
+      &-select{
+        background-color:$alice-blue;
+        padding:20px;
+        border-radius:8px;
+        border:1px solid $cool-gray;
+        box-shadow:0 2px 2px 1px $cool-gray;
+
+        .bill-state{
+          display:flex;
+          justify-content:space-between;
+          align-items:center;
+          padding-block:20px;
+          border-bottom:1px solid $cool-gray;
+          margin-bottom:20px;
+
+          p{
+            font-weight:500;
+            color:$lapis-lazuli;
+          }
+
+          a{
+            text-decoration:underline;
+            color:$cool-gray;
+            margin-top:5px;
+            display:block;
+            cursor:pointer;
+            font-size:14px;
+            transition:all 0.2s;
+
+            &:hover{
+              color:$cardinal;
+            }
+          }
+
+          &-price{
+            font-weight: 600;
+            color:$text-color;
+          }
+        }
+
+        .add-ons{
+          display:flex;
+          justify-content: space-between;
+          align-items:center;
+          margin-bottom:20px;
+
+          &-name{
+            color:$lapis-lazuli;
+          }
+
+          &-price{
+            color:$text-color;
+            font-weight:600;
+          }
+        }
+
+        .storage{
+          display:flex;
+          justify-content: space-between;
+          align-items:center;
+          margin-bottom:20px;
+
+          &-name{
+            color:$lapis-lazuli;
+          }
+
+          &-price{
+            color:$text-color;
+            font-weight:600;
+          }
+        }
+
+      }
+
+      .total{
+          display:flex;
+          justify-content:space-between;
+          padding:20px;
+
+          span{
+            font-size:14px;
+            color:$stone-2;
+          }
+
+          p{
+            font-size:18px;
+            font-weight:600;
+            color:$text-color;
+          }
+        }
+    }
+
+    &-buttons{
+      width:100%;
+      display:flex;
+      justify-content: space-between;
+      align-content:flex-end;
+      align-items:center;
+    }
+  }
+}
+</style>
