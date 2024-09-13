f15c7a2982840eb0fcb21f3e1ee0025f3c613c6b
banuaggun <banuaggun@outlook.com>
2024-09-13 12:11:54 +0300

    Responsive making studies - 1

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -3,34 +3,39 @@
     <h1>Personal Info</h1>
     <p>Please, provide your name, email and phone number</p>
   </div>
-  <form @submit.prevent="validateForm()">
-    <div class="forms">
-      <div class="forms-area">
-        <div class="forms-area-validation">
+
+  <form class="personal-info" @submit.prevent="validateForm()">
+    <div class="personal-info-area">
+
+      <div class="personal-info-area-edit">
+
+        <div class="personal-info-area-edit-validation">
           <label for="name">Name</label>
           <span v-if="$store.state.newUser.formName.isComplete">This field is required</span>
         </div>
-        <input class="forms-area-input" :class="$store.state.newUser.formName.isComplete ? 'required' : 'forms-area-input'" type="text" id="name" v-model="name" @keydown="isTyping" placeholder="Aria Doe">
-      </div>
-      <div class="forms-area">
-        <div class="forms-area-validation">
-          <label for="name">Email</label>
-          <span v-if="$store.state.newUser.formEmail.isComplete">This field is required</span>
-        </div>
-        <input class="form-group-input forms-area-input" :class="$store.state.newUser.formEmail.isComplete ? 'required' : 'forms-area-input'" type="email" id="email" v-model="email" placeholder="Aria Doe">
-      </div>
-      <div class="forms-area">
-        <div class="forms-area-validation">
-          <label for="name">Phone</label>
-          <span v-if="$store.state.newUser.formPhone.isComplete">This field is required</span>
-        </div>
-        <input class="form-group-input forms-area-input" :class="$store.state.newUser.formPhone.isComplete ? 'required' : 'forms-area-input'" type="phone" id="number" v-model="phone" placeholder="Aria Doe">
+        <input class="personal-info-area-edit-input" type="text" id="name" :class="$store.state.newUser.formName.isComplete ? 'required' : 'personal-info-area-edit-input'" v-model="name" @keydown="isTyping" placeholder="Aria Doe">
       </div>
 
+       <div class="personal-info-area-edit">
 
+        <div class="personal-info-area-edit-validation">
+          <label for="email">Email</label>
+          <span v-if="$store.state.newUser.formName.isComplete">This field is required</span>
+        </div>
+        <input class="personal-info-area-edit-input" type="text" id="email" :class="$store.state.newUser.formEmail.isComplete ? 'required' : 'personal-info-area-edit-input'" v-model="email" @keydown="isTyping" placeholder="ariadoe@mail.com">
+      </div>
+
+       <div class="personal-info-area-edit">
+
+        <div class="personal-info-area-edit-validation">
+          <label for="phone">Phone</label>
+          <span v-if="$store.state.newUser.formName.isComplete">This field is required</span>
+        </div>
+        <input class="personal-info-area-edit-input" type="number" id="phone" :class="$store.state.newUser.formPhone.isComplete ? 'required' : 'personal-info-area-edit-input'" v-model="phone" @keydown="isTyping">
+      </div>
     </div>
 
-    <div class="forms-buttons">
+    <div class="personal-info-buttons">
       <button class="forms-buttons-secondary forms-buttons-btn" style="background-color:transparent; visibility:hidden;">
         Back
       </button>
@@ -38,9 +43,8 @@
         Next
       </button>
     </div>
-    
   </form>
-  
+
 </template>
 <script setup>
 import { computed } from 'vue'
@@ -104,60 +108,45 @@ const validateForm = () => {
   store.state.currentStep++
 }
 </script>
-
 <style lang="scss">
-@import "../styles/global/global.sass";
+@import '../styles/global/global.sass';
 
-form{
+.personal-info{
   display:flex;
-  flex-direction: column;
-  justify-content: space-between;
-  position: relative;
-  margin-bottom:0;
-  height:100%;
+  flex-direction:column;
 
-  .required{
-    border:1px solid $cardinal !important;
-  }
-
-  .forms{
+  &-area{
     display:flex;
     flex-direction: column;
-    margin-bottom:20px;
-    margin-top:30px;
-
-    &-area{
-      display: flex;
-      flex-direction: column;
-      justify-content:space-between;
-      margin:10px 0;
-      outline:none;
+    margin-top:20px;
+    &-edit{
+      margin-bottom:30px;
+      width:100%;
 
       &-validation{
         display:flex;
         justify-content:space-between;
+        margin-bottom:6px;
+
+        label{
+          color:$text-color;
+          font-weight:500;
+        }
 
         span{
           font-size:14px;
-          font-weight:500;
           color:$cardinal;
+          font-weight:500;
+          letter-spacing:0.5px;
         }
-      }
-
-      label{
-        color:$text-color;
-        font-weight:500;
-        margin-bottom:6px;
       }
 
       &-input{
         width:calc(100% - 32px);
-        padding: 8px 16px;
+        padding:8px 16px;
         border-radius:4px;
         border:1px solid $text-color;
         color:$lapis-lazuli;
-        font-weight:500;
-        font-size:1rem;
 
         &:focus{
           border:1px solid $text-color;
@@ -165,22 +154,17 @@ form{
         }
       }
     }
-  
   }
 
-  .forms-buttons{
-    width:100%;
+  &-buttons{
     display:flex;
     justify-content: space-between;
-    align-content:flex-end;
-    align-items:center;
   }
-
-
 }
 
-.required{
-  border:1px solid $cardinal;
+@media screen and (max-width:600px){
+  .personal-info-area-edit{
+    margin-bottom:15px;
+  }
 }
-
 </style>
